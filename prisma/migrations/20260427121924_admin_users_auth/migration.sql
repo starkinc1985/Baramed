63a9d245cef6fe99707369cd13ff3e49a6a9983a
Abdul Moiz <abdulmoiz485@gmail.com>
2026-05-18 15:57:53 +0500

    refractoring

--- prisma/migrations/20260427121924_admin_users_auth/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateEnum
-CREATE TYPE "UserRole" AS ENUM ('ADMINISTRATOR');
-
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "passwordHash" TEXT,
-ADD COLUMN     "role" "UserRole" NOT NULL DEFAULT 'ADMINISTRATOR';
