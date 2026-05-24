c0439022d14a23546a1508c6b0b996624e18130f
Abdul Moiz <abdulmoiz485@gmail.com>
2026-05-24 16:21:42 +0500

    Final commit deploymentfix

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,17 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.DATABASE_URL!;
-
-if (!MONGODB_URI) throw new Error("DATABASE_URL is not defined");
-
 const globalForMongoose = globalThis as unknown as {
   mongooseConn?: typeof mongoose;
   mongoosePromise?: Promise<typeof mongoose>;
 };
 
 export async function connectDB(): Promise<typeof mongoose> {
+  const uri = process.env.DATABASE_URL;
+  if (!uri) throw new Error("DATABASE_URL is not defined");
+
   if (globalForMongoose.mongooseConn) return globalForMongoose.mongooseConn;
   if (!globalForMongoose.mongoosePromise) {
-    globalForMongoose.mongoosePromise = mongoose.connect(MONGODB_URI);
+    globalForMongoose.mongoosePromise = mongoose.connect(uri);
   }
   globalForMongoose.mongooseConn = await globalForMongoose.mongoosePromise;
   return globalForMongoose.mongooseConn;
